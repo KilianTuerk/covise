e8346ea77a466a5722b533e8af9906ab9188bc60
KilianTuerk <kilian.tuerk@gmail.com>
2023-11-14 17:30:34 +0100

    refactor UrbanTempo plugin and add LOD handling

---
+++ src/OpenCOVER/plugins/hlrs/UrbanTempo/Tree.h
@@ -0,0 +1,54 @@
+/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+#ifndef _TREE_H
+#define _TREE_H
+
+#include "TreeModel.h"
+#include <string>
+#include <cover/coVRPlugin.h>
+#include <cover/coVRFileManager.h>
+#include <rapidjson/document.h>
+#include <osg/Node>
+#include <osg/ref_ptr>
+#include <gdal_priv.h>
+
+// enum Season { 
+//     Winter,
+//     Spring,
+//     Summer,
+//     Fall
+// };
+
+class Tree
+{
+public:
+    Tree(std::string);
+    ~Tree();
+    std::string speciesName;
+    double xPos;
+    double yPos;
+    float altitude;
+    double scale;
+    double height;
+    osg::Matrix transform;
+    std::shared_ptr<TreeModel> treeModel;
+    std::vector<std::shared_ptr<TreeModel>> treeModelLODs;
+    osg::ref_ptr<osg::MatrixTransform> sceneGraphNode;
+    
+    void setAltitude(const float);
+    void setScale(const double);
+    void updateTreeModel();
+    void setTransform();
+    void attachToNode(osg::Node*);
+    double getDistanceToViewer();
+    void updateSceneGraphNode();
+    void setTreeModelLODs();
+};
+
+#endif
+
